ef2c4f1f2dee7dfd7f6a427540baa8afe5333d5a
pigor <pi.gorski@gmail.com>
2022-06-20 19:08:12 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,21 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return Arrays.asList(
+                new Student(1L, "Mark", "mark@monsteruniv.edu", Gender.MALE),
+                new Student(2L, "Ala", "ala@monsteruniv.edu", Gender.FEMALE)
+        );
+    }
+}
